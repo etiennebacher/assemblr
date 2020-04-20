ad4c8fc43455b381f8518ab8c7c9e11c8de9ed37
etiennebacher <etienne.bacher@orange.fr>
2020-04-20 16:02:01 +0200

    start of server part

--- R/server.R
+++ R/server.R
@@ -1,8 +1,33 @@
 server <- function(input, output, session){
 
+  regression <- lm(mpg ~ drat + hp + disp, data =  mtcars)
+
+  table_output <- reactive({
+    HTML(
+      stargazer(
+        regression,
+        regression,
+        regression,
+
+        ### General options
+        type = "html",
+        style = input$style,
+
+        ### Title and columns options"
+        title = input$title,
+        dep.var.caption = input$caption, ## MARCHE PAS
+
+        ### Result options
+        ci = input$include_ci,
+        ci.level = input$ci_level,
+        ci.separator = input$ci_separator
+      )
+    )
+  })
+
+  ### Result
   output$stargazer <- renderUI({
-    regression <- lm(mpg ~ drat + hp + disp, data =  mtcars)
-    HTML(stargazer(regression, regression, regression, type = "html"))
+    table_output()
   })
 
   ### Close app if cancel or done

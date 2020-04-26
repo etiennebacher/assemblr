4b8896cfca8d640987e5bb8e8bf95b6e39fddd9e
etiennebacher <etienne.bacher@orange.fr>
2020-04-26 14:55:41 +0200

    add scrollbar in collapsePanel

--- R/ui.R
+++ R/ui.R
@@ -15,51 +15,51 @@ ui <- function(){
   miniUI::miniTabstripPanel(
     miniUI::miniTabPanel("Table Options", icon = shiny::icon("table"),
                          miniUI::miniContentPanel(
-                   # shiny::selectInput("choose_format",
-                   #             "Choose the output format",
-                   #             choices = c("HTML", "LaTeX"),
-                   #             selected = "LaTeX"),
+                           # shiny::selectInput("choose_format",
+                           #             "Choose the output format",
+                           #             choices = c("HTML", "LaTeX"),
+                           #             selected = "LaTeX"),
                            shiny::selectInput("choose_regressions",
                                               "Choose the regressions in the table",
                                               choices = names(list_regressions),
                                               multiple = TRUE,
                                               selected = names(list_regressions)),
-                   shiny::fillRow(
-                     flex = c(1, 0.5, 2),
-                     shiny::fillCol(
-                       shinyBS::bsCollapse(
-                         id = "latex_options",
-                         general_options(),
-                         title_and_columns_options(),
-                         result_options(),
-                         additional_info_options(),
-                         footnote_options()
-                       )
-                       ),
-                     shiny::fillCol(),
-                     shiny::fillCol(
-                       shiny::uiOutput("stargazer")
-                      )
-                   )
-                   )
-                 ),
+                           shiny::fillRow(
+                             flex = c(0.5, 0.2, 0.5),
+                             shiny::fillCol(
+                               style = "overflow-y:auto; max-height: 600px",
+                               shinyBS::bsCollapse(
+                                 id = "latex_options",
+                                 general_options(),
+                                 title_and_columns_options(),
+                                 result_options(),
+                                 additional_info_options(),
+                                 footnote_options()
+                               )
+                             ),
+                             shiny::fillCol(),
+                             shiny::fillCol(shiny::uiOutput("stargazer"))
+                           )
+                         )
+    ),
     miniUI::miniTabPanel("Code", icon = shiny::icon("code")),
-    miniUI::miniTabPanel("About", icon = shiny::icon("info"),
-                miniUI::miniContentPanel(
-                  shiny::fillRow(
-                     flex = c(1, 2, 1),
-                     shiny::fillCol(),
-                     shiny::fillCol(
-                       shiny::wellPanel(
-                         shiny::p("The goal of this addin is to build {stargazer} tables more easily. At the beginning, choose if you want an HTML or a LaTeX table (note however that some options are only available with LaTeX output).",
-                                  shiny::a("This cheatsheet", href = "https://www.jakeruss.com/cheatsheets/stargazer/"), "by Jake Russ has been extremely useful to build this addin.")
-                       )
-                     ),
-                     shiny::fillCol()
-                   )
-                )
-    )
+    miniUI::miniTabPanel("About",
+                         icon = shiny::icon("info"),
+                         miniUI::miniContentPanel(
+                           shiny::fillRow(
+                             flex = c(1, 2, 1),
+                             shiny::fillCol(),
+                             shiny::fillCol(shiny::wellPanel(
+                               shiny::p(
+                                 "The goal of this addin is to build {stargazer} tables more easily. At the beginning, choose if you want an HTML or a LaTeX table (note however that some options are only available with LaTeX output).",
+                                 shiny::a("This cheatsheet", href = "https://www.jakeruss.com/cheatsheets/stargazer/"),
+                                 "by Jake Russ has been extremely useful to build this addin."
+                               )
+                             )),
+                             shiny::fillCol()
+                           )
+                         ))
 
   )
-)
+  )
 }
